d507f7e5e9d8d7b6ca28cfd5b763befdc054f6c2
Cory Sabol <cory@secureideas.com>
2019-03-24 20:39:33 -0400

    docker user to root backdoor installer/bootstrap

---
+++ target_install/docker_user_2_root.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# create a normal user and add them to the docker group
+echo "adding docker_user_2_root user: $DOCKER_PRIV_ESC_USER..."
+useradd $DOCKER_PRIV_ESC_USER -s /bin/bash -m -G docker
+echo $DOCKER_PRIV_ESC_USER':password' | chpasswd
+
